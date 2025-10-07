3df919df4fa53fb4bd16b23b97852de2701acfb1
Camila Rodriguez <rmariacamila828@gmail.com>
2025-10-06 20:25:09 -0500

    chore( update datebase connection path

--- Backend-MediKeep/Database/Conexion.js
+++ Backend-MediKeep/Database/Conexion.js
@@ -1,7 +1,9 @@
 const { MongoClient, ObjectId } = require("mongodb");
 class Conexion {
   constructor() { 
-     this.uri = "mongodb://localhost:27017";
+     //this.uri = "mongodb://localhost:27017";
+    this.uri = "mongodb://camila-server:MZHn2BynqRToyiKmNIc4cizUwveelwFevfpfOSwWqVcmgTXcy6P9UTHj5lkSvznv1EmvSNJzpBvzACDbcxBDew%3D%3D@camila-server.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@camila-server@";
+     
      this.client = new MongoClient(this.uri);
   }
     
